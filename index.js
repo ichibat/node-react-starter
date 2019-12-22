2c0da8378f30287ff1216d1150f17524ec1e4b89
tamotsu ichiba <ichibat@gmail.com>
2019-12-22 13:51:47 +0900

    second commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://tims:Hit135Run@ds019946.mlab.com:19946/ktmethod", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`)
+});
